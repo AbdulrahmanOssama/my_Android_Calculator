acef74122053eddda7874627d1ac6e3b784ebbb1
AbdulrahmanOssama <44143250+AbdulrahmanOssama@users.noreply.github.com>
2019-03-11 12:48:48 +0200

    Create src.java

---
+++ src.java
@@ -0,0 +1,13 @@
+package com.example.aswitch; //"aswitch", is the name of the project... not the best tho :P 
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
